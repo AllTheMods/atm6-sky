e0ee4714e0c61655138bc35b27b6504909e82049
Starforcraft <starforcraft@yahoo.com>
2021-04-28 21:49:49 +0200

    Centrifuge Tiers & Creative Apiary Recipes

--- kubejs/server_scripts/creative.js
+++ kubejs/server_scripts/creative.js
@@ -20,6 +20,62 @@ events.listen('recipes', function(e) {
             }]
         })
     }
+    //Ctiers
+    e.shaped('3x ctiers:centrifuge_casing_tier_creative', [
+        'IRG',
+        'RTR',
+        'DRE'
+    ], {
+        I: 'compressium:iron_3',
+        R: 'compressium:redstone_3',
+        G: 'compressium:gold_3',
+        T: 'ctiers:centrifuge_casing_tier_5',
+        D: 'compressium:diamond_3',
+        E: 'compressium:emerald_3'
+    })
+    e.shaped('ctiers:centrifuge_controller_tier_creative', [
+        'ICG',
+        'HSH',
+        'DHE'
+    ], {
+        I: 'compressium:iron_5',
+        C: 'ctiers:centrifuge_casing_tier_creative',
+        G: 'compressium:gold_5',
+        S: atm_star,
+        D: 'compressium:diamond_5',
+        E: 'compressium:emerald_5',
+        H: 'compressium:honey_5'
+    })
+    //CApiary
+    e.shaped('creativeapiary:tcreative_apiary', [
+        'HMH',
+        'MCM',
+        'TAT'
+    ], {
+        H: 'compressium:honey_6',
+        M: 'mekanism:pellet_antimatter',
+        C: 'ctiers:centrifuge_controller_tier_creative',
+        T: 'resourcefulbees:t4_apiary',
+        A: 'allthemodium:unobtainium_allthemodium_alloy_block'
+    })
+    e.shaped('creativeapiary:creative_apiary_storage', [
+        'HNH',
+        'CAC',
+        'NHN'
+    ], {
+        H: 'compressium:honey_3',
+        N: 'kubejs:nether_star_block',
+        C: 'ctiers:centrifuge_casing_tier_creative',
+        A: 'resourcefulbees:apiary_storage'
+    })
+    e.shaped('creativeapiary:creative_apiary_breeder', [
+        'BNB',
+        'NBN',
+        'BNB'
+    ], {
+        B: 'resourcefulbees:apiary_breeder',
+        N: 'kubejs:nether_star_block'
+    })
     //Exchangers
     e.shaped('exchangers:creative_exchanger', [
         ' E ',

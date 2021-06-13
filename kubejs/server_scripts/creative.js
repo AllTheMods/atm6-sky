01b8ef228bd74a477a981155e6bafad054cb9eb4
Lady Lexxie <merideo041096@gmail.com>
2021-06-13 06:08:33 +0100

    Fixed Energizing KubeJS Scripts

--- kubejs/server_scripts/creative.js
+++ kubejs/server_scripts/creative.js
@@ -146,11 +146,11 @@ events.listen('recipes', e => {
   })
 
   //Immersive Engineering
-  energize(['immersiveengineering:capacitor_lv', 'immersiveengineering:capacitor_mv', 'immersiveengineering:capacitor_hv', atm_star], 'immersiveengineering:capacitor_creative', 2147483647)
+  energize(['immersiveengineering:capacitor_lv', 'immersiveengineering:capacitor_mv', 'immersiveengineering:capacitor_hv', atm_star], 'immersiveengineering:capacitor_creative', "2147483647")
   //Integrated Dynamics
-  energize(['integrateddynamics:coal_generator', 'integrateddynamics:coal_generator', atm_star, 'integrateddynamics:coal_generator', 'integrateddynamics:coal_generator'], 'integrateddynamics:energy_battery_creative', 2147483647)
+  energize(['integrateddynamics:coal_generator', 'integrateddynamics:coal_generator', atm_star, 'integrateddynamics:coal_generator', 'integrateddynamics:coal_generator'], 'integrateddynamics:energy_battery_creative', "2147483647")
   //Applied Energistics 2
-  energize(['appliedenergistics2:dense_energy_cell', atm_star], 'appliedenergistics2:creative_energy_cell', 2147483647)
+  energize(['appliedenergistics2:dense_energy_cell', atm_star], 'appliedenergistics2:creative_energy_cell', "2147483647")
   //Mekanism
   e.shaped('mekanism:creative_energy_cube', [
     'ATA',
@@ -170,7 +170,7 @@ events.listen('recipes', e => {
         stored: "18446744073709551615.9999"
       }]
     }
-  }), 2147483647)
+  }), "2147483647")
 
   //Create
   e.recipes.create.mechanical_crafting('create:creative_motor', [
@@ -186,12 +186,12 @@ events.listen('recipes', e => {
   })
 
   //Refined Storage
-  energize(['refinedstorage:wireless_grid'], 'refinedstorage:creative_wireless_grid', 2147483647)
-  energize(['refinedstorage:wireless_fluid_grid'], 'refinedstorage:creative_wireless_fluid_grid', 2147483647)
-  energize(['refinedstorage:wireless_crafting_monitor'], 'refinedstorage:creative_wireless_crafting_monitor', 2147483647)
-  energize(['refinedstorageaddons:wireless_crafting_grid'], 'refinedstorageaddons:creative_wireless_crafting_grid', 2147483647)
-  energize(['refinedstorage:portable_grid'], 'refinedstorage:creative_portable_grid', 2147483647)
-  energize(['refinedstorage:controller', atm_star], 'refinedstorage:creative_controller', 2147483647)
+  energize(['refinedstorage:wireless_grid'], 'refinedstorage:creative_wireless_grid', "2147483647")
+  energize(['refinedstorage:wireless_fluid_grid'], 'refinedstorage:creative_wireless_fluid_grid', "2147483647")
+  energize(['refinedstorage:wireless_crafting_monitor'], 'refinedstorage:creative_wireless_crafting_monitor', "2147483647")
+  energize(['refinedstorageaddons:wireless_crafting_grid'], 'refinedstorageaddons:creative_wireless_crafting_grid', "2147483647")
+  energize(['refinedstorage:portable_grid'], 'refinedstorage:creative_portable_grid', "2147483647")
+  energize(['refinedstorage:controller', atm_star], 'refinedstorage:creative_controller', "2147483647")
 
   //Pneumatic Craft
   pressure([{
